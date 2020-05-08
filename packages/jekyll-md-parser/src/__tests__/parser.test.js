7416242159cdaa08cbaf9cc85d08357a1357b108
James Calcaben <jcalcaben+github@gmail.com>
2020-05-07 23:45:19 -0500

    Make liquid tokenizer return a definition node

--- packages/jekyll-md-parser/src/__tests__/parser.test.js
+++ packages/jekyll-md-parser/src/__tests__/parser.test.js
@@ -2,6 +2,8 @@ const unified = require('unified')
 const parser = require('remark-parse')
 const vfile = require('vfile');
 
+const markdownParser = require('../parser');
+
 test("Parse normal markdown", () => {
     const input = `
 # Header 1
@@ -13,7 +15,28 @@ Hello **World**!`
         contents: input
     })
 
-    const tree = unified().use(parser).parse(file);
+    const tree = unified().use(parser).use(markdownParser).parse(file);
+
+    expect(tree).toMatchSnapshot();
+})
+
+test("Parse markdown with liquid link", () => {
+    const input = `
+# Header 1
+
+This is a [reference link][] example.
+
+This is [another reference link][] example.
+
+[reference link]: {%link link/to/a/markdown/file.md %}
+[another reference link]: {% link link/to/another/file.md %}`
+
+    const file = vfile({
+        path: './example.md',
+        contents: input
+    })
+
+    const tree = unified().use(parser).use(markdownParser).parse(file);
 
     expect(tree).toMatchSnapshot();
 })
--- packages/jekyll-md-parser/src/parser.js
+++ packages/jekyll-md-parser/src/parser.js
@@ -1,5 +1,39 @@
-const parser = ()=>{
+function parser(config) {
+    const Parser = this.Parser
+    const blockTokenizers = Parser.prototype.blockTokenizers;
+    let blockMethods = Parser.prototype.blockMethods
+
+    blockTokenizers.liquid = tokenizeLiquid;
+
+    blockMethods.splice(blockMethods.indexOf('definition'), 0, 'liquid');
+}
+
+const tokenizeLiquid = (eat, value, silent) => {
+    var match = /^\[([^\]]+)\]:\s?\{%\s?link ([^\s]+) %\}/.exec(value);
+
+    if(match){
+        if(silent){
+            return true;
+        }
+
+        return(eat(match[0])({
+            type: 'definition',
+            identifier: match[1],
+            label: match[1],
+            url: match[2],
+            title: match[1]
+
+        }))
+    }
 
 }
+
+const locateLiquid = (value, fromIndex) => {
+    return value.indexOf('[', fromIndex);
+}
+
+tokenizeLiquid.notInLink = false
+tokenizeLiquid.locator = locateLiquid
+
 
 module.exports = parser;
